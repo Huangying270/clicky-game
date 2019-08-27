64324f68c7a787ae15b6396aa4530b7ff3cec998
Ying Huang <huangying270@gmail.com>
2019-08-27 02:21:16 -0400

    App js line 8 Component undefined

--- clicky-game/src/App.js
+++ clicky-game/src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Card from "./components/Card";
+import Wrapper from "./components/Wrapper";
+import Header from "./components/Header";
+import cards from "./cards.json";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    cards,
+    score: 0,
+    highscore: 0
+  };
+
+  endGame = () => {
+    if (this.state.score > this.state.highscore) {
+      this.setState({highscore: this.state.score}, function() {
+        console.log(this.state.highscore);
+      });
+    };
+    this.state.cards.forEach(card => {
+      card.count = 0;
+    });
+    alert("Game Over");
+    this.setState({score: 0});
+    return true;
+  }
+
+  clickCount = id => {
+    this.state.cards.find((res, data) => {
+      if (res.id === id) {
+        if (cards[data].count === 0) {
+          cards[data].count = cards[data].count + 1;
+          this.setState({score: this.state.score + 1}, function() {
+            console.log(this.state.score);
+          });
+          this.state.cards.sort(() => Math.random() - 0.5)
+          return true;
+        } else {
+          this.endGame();
+        };
+      };
+    });
+  };
+
+  render() {
+    return (
+      <Wrapper>
+        <Header score={this.state.score} highscore={this.state.highscore}>Clicky Game</Header>
+        {this.state.cards.map(card => (
+          <Card
+          clickCount={this.clickCount}
+          id={card.id}
+          key={card.id}
+          image={card.image}
+          />
+        ))}
+      </Wrapper>
+    )
+  }
 }
 
 export default App;
--- clicky-game/src/components/Card/card.js
+++ clicky-game/src/components/Card/card.js
@@ -2,7 +2,11 @@ import React from "react";
 import "./style.css";
 
 function Card(props) {
-    
+    <div className="card" onClick={() => props.clickCount(props.id)}>
+        <div className="img-container">
+            <img alt={props.name} src={props.image}/>
+        </div>
+    </div>
 }
 
 
